2f1e1c8bfa2c124e17e1eee1a9cea835424d4346
sunjing <sun.jing22@zte.com.cn>
2016-04-06 14:14:21 +0800

    use gunicorn

--- myDaisyCloud/mysite/mysite/settings.py
+++ myDaisyCloud/mysite/mysite/settings.py
@@ -72,6 +72,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    
+    '/root/test/myDaisyCloud/mysite/daisycloud/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -112,7 +114,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     
-    'E:\myDaisyCloud\mysite\daisycloud\templates',
+    '/root/test/myDaisyCloud/mysite/daisycloud/templates',
 )
 
 INSTALLED_APPS = (
