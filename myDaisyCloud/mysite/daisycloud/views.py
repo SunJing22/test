0d53291e909cda60946a19592934d5a0654755e1
SunJing <sun.jing22@zte.com.cn>
2016-04-13 09:39:08 +0800

    modify templates

--- myDaisyCloud/mysite/daisycloud/views.py
+++ myDaisyCloud/mysite/daisycloud/views.py
@@ -1,8 +1,8 @@
 # coding:utf-8
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 
 def index(request):
-    #return render_to_response('test.html')
-    return render_to_response('joinin.html')
+    return render_to_response('index.html')
